63c0567dcdadfe0f3903a5d743ce3adb6ef97a66
AlexandarKruskovity <kruskovityalexandar@turr.hu>
2026-01-26 12:11:42 +0100

    asd

---
+++ AutoKolcsozesX/model/kolcsonzes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKolcsozesX.model
+{
+    internal class kolcsonzes
+    {
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public double year { get; set; }
+        public string vin { get; set; }
+        
+    }
+    public kolcsonzes(int id, string brand, string model, string color, int year, string vin)
+    {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.vin = vin;
+
+    }
+    public kolcsonzes() 
+        {
+
+        }
+   
+}
